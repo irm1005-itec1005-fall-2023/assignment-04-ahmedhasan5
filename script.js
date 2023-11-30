cc7b6981e29531d63eccc35a82d4d8a9f25b762e
ahmedhasan5 <ahmedhasan3@cmail.carleton.ca>
2023-11-29 22:42:30 -0500

    Nearly there

--- script.js
+++ script.js
@@ -4,40 +4,96 @@
  *
  */
 
+//Variables
+let form = document.getElementById("form-superhero");
 
-//
-// Variables
-//
+let inputName = document.getElementById("name-input-1")
 
-// Constants
-const appID = "app";
-const headingText = "This is my ToDoList";
+let myList = document.getElementById("name-list")
 
-// DOM Elements
-let appContainer = document.getElementById(appID);
-//
+let tempName = document.createElement("li")
+
+let counterP = document.getElementById("ptag")
+
+
+
+//Add an Array
+let nameArray = []
+
+//Submit Form
+form.addEventListener("submit", ClickedSubmit);
 
 // Functions
-//
+function ClickedSubmit(event){
 
-// Add a heading to the app container
-function inititialise() {
-  // If anything is wrong with the app container then end
-  if (!appContainer) {
-    console.error("Error: Could not find app contianer");
-    return;
-  }
+  //PREVENT THE DEFAULT BEHAVIOR OF THE FORM
+  event.preventDefault()
 
-  // Create an h1 and add it to our app
-  const h1 = document.createElement("h1");
-  h1.innerText = headingText;
-  appContainer.appendChild(h1);
+  
+  //CONSOLE LOG THE VALUE OFTHE INPUT
+  console.log("USER ENTERED:" , inputName.value);
 
-  // Init complete
-  console.log("App successfully initialised");
+ 
+
+  //Get the value of the input
+  const inputvalue = inputName.value
+
+  //Check that inputValue is not empty 
+  nameArray.push(inputvalue);
+  inputName.value = ""; 
+  renderlist();
 }
 
-//
-// Inits & Event Listeners
-//
-inititialise();
+function renderlist(){
+  console.log("Render List:", nameArray);
+
+  myList.innerHTML = "";
+  for( let i = 0; i < nameArray.length; i++){
+    let tempName = document.createElement("li")
+    tempName.textContent = nameArray[i];
+
+
+    let deleteButton = document.createElement("button");
+
+    deleteButton.textContent = "❌"
+
+    tempName.appendChild(deleteButton)
+
+    myList.appendChild(tempName);
+
+    deleteButton.addEventListener("click", function () {
+      nameArray.splice(i, 1);
+      renderlist();
+    })
+
+    let completeButton = document.createElement("button");
+
+
+    completeButton.textContent = "✅"
+
+    tempName.appendChild(completeButton)
+
+    myList.appendChild(tempName);
+
+    completeButton.addEventListener("click", function () {
+      nameArray[i].completed = 
+      !nameArray[i].completed;
+     
+    });
+     
+    if (nameArray[i].completed) {
+      tempName.style.textDecoration = "line-through";
+    }
+  }
+ 
+  if( nameArray.length === 0) {
+  counterP.textContent = "NO TASK ON THE LIST";
+  }
+  else {
+    counterP.textContent = "You have " + nameArray.length +  " task(s) on the list";
+  }
+
+}
+
+
+renderlist()
